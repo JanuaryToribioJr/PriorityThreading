fadb767d46be8f2b88d372f146b1b8c4daee5e9b
JanuaryToribioJr <toribiojanuaryjr@gmail.com>
2024-11-21 23:11:59 +0800

    Add project files.

---
+++ PriorityThreading/frmTrackThread.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PriorityThreading
+{
+    partial class frmTrackThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTrackThread));
+            this.ThreadLabel = new System.Windows.Forms.Label();
+            this.runBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ThreadLabel
+            // 
+            this.ThreadLabel.AutoSize = true;
+            this.ThreadLabel.BackColor = System.Drawing.Color.SlateBlue;
+            this.ThreadLabel.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThreadLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ThreadLabel.Location = new System.Drawing.Point(38, 51);
+            this.ThreadLabel.Name = "ThreadLabel";
+            this.ThreadLabel.Size = new System.Drawing.Size(250, 27);
+            this.ThreadLabel.TabIndex = 0;
+            this.ThreadLabel.Text = "- Thread Starts -";
+            // 
+            // runBtn
+            // 
+            this.runBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("runBtn.BackgroundImage")));
+            this.runBtn.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.runBtn.Location = new System.Drawing.Point(117, 108);
+            this.runBtn.Name = "runBtn";
+            this.runBtn.Size = new System.Drawing.Size(91, 28);
+            this.runBtn.TabIndex = 1;
+            this.runBtn.Text = "RUN";
+            this.runBtn.UseVisualStyleBackColor = true;
+            this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
+            // 
+            // frmTrackThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(326, 184);
+            this.Controls.Add(this.runBtn);
+            this.Controls.Add(this.ThreadLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "frmTrackThread";
+            this.Text = "frmTrackThread";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ThreadLabel;
+        private System.Windows.Forms.Button runBtn;
+    }
+}
+
---
+++ PriorityThreading/frmTrackThread.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PriorityThreading
+{
+    public partial class frmTrackThread : Form
+    {
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+        Thread threadA = new Thread(MyThreadClass.Thread1);
+        Thread threadB = new Thread(MyThreadClass.Thread2);
+        Thread threadC = new Thread(MyThreadClass.Thread1);
+        Thread threadD = new Thread(MyThreadClass.Thread2);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+            threadC.Name = "Thread C";
+            threadD.Name = "Thread D";
+
+            threadA.Priority = ThreadPriority.Highest;
+            threadB.Priority = ThreadPriority.Normal;
+            threadC.Priority = ThreadPriority.AboveNormal;
+            threadD.Priority = ThreadPriority.BelowNormal;
+        }
+
+        private void runBtn_Click(object sender, EventArgs e)
+        {
+            ThreadLabel.Text = "- Thread Starts -";
+            Console.WriteLine("- Thread Starts -");
+
+            threadA.Start();
+            threadC.Start();
+            threadB.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            ThreadLabel.Text = "- End of Thread -";
+            Console.WriteLine("- End of Thread -");
+        }
+    }
+    //
+    public class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            Thread thread = Thread.CurrentThread;
+            for (int a = 0; a <= 2; a++)
+            {               
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + a);
+                Thread.Sleep(500);
+            }
+        }
+        public static void Thread2()
+        {
+            
+            for (int a = 0; a <= 5 ; a++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + a);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+
+}
